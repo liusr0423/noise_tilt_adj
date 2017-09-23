476f8f5b8952071ae5a075e7e06f3264c44fedb8
Sirui Liu <liusr0423@users.noreply.github.com>
2017-09-23 00:08:06 -0400

    added debug screen mode

--- functions/prepScreen.m
+++ functions/prepScreen.m
@@ -9,7 +9,9 @@ HideCursor;
 
 scr.subDist = 57;   % subject distance (cm)
 scr.colDept = 32;
-scr.width   = 330;%410;  % monitor width (mm) (435 desk monitor)
+scr.width = 340; %410;  % monitor width (mm) (435 desk monitor)
+scr.xres  = 1024;
+scr.yres  = 768;
 
 % If there are multiple displays guess that one without the menu bar is the
 % best choice.  Dislay 0 has the menu bar.
@@ -28,10 +30,10 @@ Screen('Preference', 'VisualDebugLevel',3);
 
 % Open a window.  Note the new argument to OpenWindow with value 2, specifying the number of buffers to the onscreen window.
 imagingMode = kPsychNeed32BPCFloat;
-[scr.main,scr.rect] = Screen('OpenWindow',scr.expScreen, [0.5 0.5 0.5],[],scr.colDept,2,0,2,imagingMode);
-
-% get information about  screen
-[scr.xres, scr.yres]    = Screen('WindowSize', scr.main);       % heigth and width of screen [pix]
+[scrx, scry] = Screen('WindowSize',  0);
+%[scr.main,scr.rect] = Screen('OpenWindow',scr.expScreen, [0.5 0.5 0.5],[],scr.colDept,2,0,2,imagingMode);
+[scr.main,scr.rect] = PsychImaging('OpenWindow',0,WhiteIndex(scr.expScreen)/2, [scrx-scr.xres,scry-scr.yres,scrx,scry], scr.colDept,2,...
+    [], [],  imagingMode);
 
 % determine th main window's center
 [scr.centerX, scr.centerY] = WindowCenter(scr.main);
